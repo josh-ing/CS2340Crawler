38451984103c02a50e4c776f26ed306f435e4fec
jzheng323 <jzheng323@gatech.edu>
2021-02-28 20:16:12 -0500

    Create tests for main menu screen

---
+++ src/tests/MainScreenTest.java
@@ -0,0 +1,26 @@
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import quack.controllers.MainMenuController;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class MainScreenTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        MainMenuController controller = new MainMenuController(stage);
+        controller.initMainMenu();
+
+    }
+
+    @Test
+    public void testPlayButton() {
+        verifyThat("Play", NodeMatchers.isNotNull());
+    }
+
+
+}
