721aa3bba1b51ad8afde00fd07a65e793624f787
Justin Chau <zyugaloeng@gmail.com>
2021-03-31 23:31:58 -0400

    Add weapon types.

---
+++ src/main/java/quack/models/weapons/KnifeWeapon.java
@@ -0,0 +1,8 @@
+package quack.models.weapons;
+
+public class KnifeWeapon extends Weapon {
+
+    public KnifeWeapon() {
+        super(1, 1, 10, 10);
+    }
+}
---
+++ src/main/java/quack/models/weapons/LongSwordWeapon.java
@@ -0,0 +1,8 @@
+package quack.models.weapons;
+
+public class LongSwordWeapon extends Weapon {
+
+    public LongSwordWeapon() {
+        super(1, 3, 10, 5);
+    }
+}
