828785e0b46b3ef6d18e6d9c59acb6fc6bc83921
Ng, Joshua Zhan Tao <jng45@gatech.edu>
2021-03-31 22:55:25 -0400

    image and dungeon image logic

--- src/main/java/quack/models/tilesets/TileSet.java
+++ src/main/java/quack/models/tilesets/TileSet.java
@@ -3,17 +3,29 @@ package quack.models.tilesets;
 import javafx.scene.image.Image;
 import quack.models.Room;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public abstract class TileSet {
 
     private Image FLOOR;
     private Image WALL;
     private Image PORTAL;
 
-    public TileSet(String floor, String wall, String portal) {
-        //Set FLOOR to be the image loaded from the directory floor.
+    public TileSet(String floor, String wall, String portal) throws FileNotFoundException {
+        FLOOR = new Image(new FileInputStream(floor));
+        WALL = new Image(new FileInputStream(wall));
+        PORTAL = new Image(new FileInputStream(portal));
     }
 
     public Image getTileImage(Room.RoomCellType cellType) {
+        if (cellType == Room.RoomCellType.FLOOR) {
+            return FLOOR;
+        } else if (cellType == Room.RoomCellType.WALL) {
+            return WALL;
+        } else {
+            return PORTAL;
+        }
         //Logic to return the correct image based on cellType
     }
 }
