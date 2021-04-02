f6a46fc781c0bda15bd6c243e10062da47b25c78
Ng, Joshua Zhan Tao <jng45@gatech.edu>
2021-04-01 22:58:44 -0400

    character image unit tests creation

--- src/main/java/quack/models/characters/HenryCharacter.java
+++ src/main/java/quack/models/characters/HenryCharacter.java
@@ -3,8 +3,14 @@ package quack.models.characters;
 import java.io.FileNotFoundException;
 
 public class HenryCharacter extends Character {
+    private String asset;
 
     public HenryCharacter()  {
         super(2000, 10, 10, "src/main/resources/assets/characters/henrychar.png");
+        this.asset = "src/main/resources/assets/characters/henrychar.png";
+    }
+
+    public String getAsset() {
+        return asset;
     }
 }
--- src/main/java/quack/models/characters/PelicanCharacter.java
+++ src/main/java/quack/models/characters/PelicanCharacter.java
@@ -1,8 +1,14 @@
 package quack.models.characters;
 
 public class PelicanCharacter extends Character {
+    private String asset;
 
     public PelicanCharacter() {
         super(2000, 10, 10, "src/main/resources/assets/characters/pelicanchar.png");
+        this.asset = "src/main/resources/assets/characters/pelicanchar.png";
+    }
+
+    public String getAsset() {
+        return asset;
     }
 }
