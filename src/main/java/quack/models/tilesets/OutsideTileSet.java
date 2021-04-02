0dea3cbe6eba5fd87aedd5507b92f06db0502e15
Justin Chau <zyugaloeng@gmail.com>
2021-04-01 23:25:04 -0400

    Add better tileset.

--- src/main/java/quack/models/tilesets/OutsideTileSet.java
+++ src/main/java/quack/models/tilesets/OutsideTileSet.java
@@ -5,7 +5,18 @@ import java.io.FileNotFoundException;
 public class OutsideTileSet extends TileSet {
 
     public OutsideTileSet() {
-        super("src/main/resources/assets/tiles/dungeon_floor.jpg", "src/main/resources/assets/tiles/dungeon_wall.jpg"
-        , "src/main/resources/assets/tiles/portal.jpg");
+        super(
+                "src/main/resources/assets/tiles/outside/tile100.png",
+                "src/main/resources/assets/tiles/outside/tile098.png",
+                "src/main/resources/assets/tiles/outside/tile103.png",
+                "src/main/resources/assets/tiles/outside/tile080.png",
+                "src/main/resources/assets/tiles/outside/tile118.png",
+                "src/main/resources/assets/tiles/outside/tile079.png",
+                "src/main/resources/assets/tiles/outside/tile084.png",
+                "src/main/resources/assets/tiles/outside/tile117.png",
+                "src/main/resources/assets/tiles/outside/tile122.png",
+                "src/main/resources/assets/tiles/outside/tile031.png",
+                "src/main/resources/assets/tiles/outside/tile031.png"
+        );
     }
 }
