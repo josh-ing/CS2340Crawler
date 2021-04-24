416d276110cfa2c39288b97fe2a3a8319ea9ac7e
Ng, Joshua Zhan Tao <jng45@gatech.edu>
2021-04-23 23:44:33 -0400

    change boss attack radius and boss movement logic

--- src/main/java/quack/models/monsters/Boss.java
+++ src/main/java/quack/models/monsters/Boss.java
@@ -1,69 +1,53 @@
 package quack.models.monsters;
-//import breath first
 
 import quack.models.GameState;
+import quack.models.Player;
 import quack.models.Position;
 import quack.models.Room;
+
+import java.util.ArrayList;
 
 public class Boss extends Monster {
     public Boss() { super(200, 20, 2, "src/main/resources/assets/monsters/boss.gif" ); }
 
     @Override
     public void update() {
-        this.movement();
-        super.randomAttack();
+        movement();
+        bossAttack();
+    }
+
+    public void bossAttack() {
+        //boss has slightly bigger attack radius
+        //each side has a hitbox 2 blocks wide
+        ArrayList<Position> attackPositions = new ArrayList<>();
+        //right
+        attackPositions.add(getPosition().translateRight());
+        attackPositions.add(getPosition().translateRight().translateRight());
+        //down
+        attackPositions.add(getPosition().translateDown());
+        attackPositions.add(getPosition().translateDown().translateDown());
+        //left
+        attackPositions.add(getPosition().translateLeft());
+        attackPositions.add(getPosition().translateLeft().translateLeft());
+        //up
+        attackPositions.add(getPosition().translateUp());
+        attackPositions.add(getPosition().translateUp().translateUp());
+
+        Player player = GameState.getInstance().getPlayer();
+
+        for (Position attackPosition : attackPositions) {
+            if (player.getPosition().equals(attackPosition)) {
+                attack(player);
+            }
+        }
     }
 
     public void movement() {
         Room currentRoom = GameState.getInstance().getCurrentRoom();
         Rotation[] rotations = Rotation.values();
-        Rotation nextMove = null;
+        Rotation nextMove = getAdjacent(currentRoom, rotations);
         Position nextPosition = null;
-        int upDistance = 0;
-        int downDistance = 0;
-        int leftDistance = 0;
-        int rightDistance = 0;
-        Position currentPos = this.getPosition();
-        Position playerPos = GameState.getInstance().getPlayer().getPosition();
-        if (currentRoom.isValidPosition(currentPos.translateUp())) {
-            Position adjacentUp = currentPos.translateUp();
-            upDistance = (adjacentUp.getRow() - playerPos.getRow()) + (adjacentUp.getCol() - playerPos.getCol());
-        }
-        if (currentRoom.isValidPosition(currentPos.translateDown())) {
-            Position adjacentDown = currentPos.translateDown();
-            downDistance = (adjacentDown.getRow() - playerPos.getRow()) + (adjacentDown.getCol() - playerPos.getCol());
-        }
-        if (currentRoom.isValidPosition(currentPos.translateLeft())) {
-            Position adjacentLeft = currentPos.translateLeft();
-            leftDistance = (adjacentLeft.getRow() - playerPos.getRow()) + (adjacentLeft.getCol() - playerPos.getCol());
-        }
-        if (currentRoom.isValidPosition(currentPos.translateRight())) {
-            Position adjacentRight = currentPos.translateDown();
-            rightDistance = (adjacentRight.getRow() - playerPos.getRow()) + (adjacentRight.getCol() - playerPos.getCol());
-        }
 
-        int[] adjacentArray = new int[]{upDistance, downDistance, leftDistance, rightDistance};
-        int small = Integer.MAX_VALUE;
-        int smallestIndex = 0;
-        for (int i = 0; i < adjacentArray.length; i++) {
-            if (adjacentArray[i] < small) {
-                small = adjacentArray[i];
-                smallestIndex = i;
-            }
-        }
-        if (smallestIndex == 1) {
-            //up
-            nextMove = rotations[1];
-        } else if (smallestIndex == 3) {
-            //down
-            nextMove = rotations[3];
-        } else if (smallestIndex == 2) {
-            //left
-            nextMove = rotations[2];
-        } else if (smallestIndex == 0) {
-            //right
-            nextMove = rotations[0];
-        }
         switch (nextMove) {
             case RIGHT:
                 nextPosition = getPosition().translateRight();
@@ -84,5 +68,57 @@ public class Boss extends Monster {
         if (currentRoom.isValidPosition(nextPosition)) {
             setPosition(nextPosition);
         }
+    }
+
+    private Rotation getAdjacent(Room currentRoom, Rotation[] rotations) {
+        int upDistance = 0;
+        int downDistance = 0;
+        int leftDistance = 0;
+        int rightDistance = 0;
+        Position currentPos = getPosition();
+        Player player = GameState.getInstance().getPlayer();
+        Position playerPos = player.getPosition();
+        if (currentRoom.isValidPosition(currentPos.translateUp())) {
+            Position adjacentUp = currentPos.translateUp();
+            upDistance = Math.abs(adjacentUp.getRow() - playerPos.getRow()) + Math.abs(adjacentUp.getCol() - playerPos.getCol());
+        }
+        if (currentRoom.isValidPosition(currentPos.translateDown())) {
+            Position adjacentDown = currentPos.translateDown();
+            downDistance = Math.abs(adjacentDown.getRow() - playerPos.getRow()) + Math.abs(adjacentDown.getCol() - playerPos.getCol());
+        }
+        if (currentRoom.isValidPosition(currentPos.translateLeft())) {
+            Position adjacentLeft = currentPos.translateLeft();
+            leftDistance = Math.abs(adjacentLeft.getRow() - playerPos.getRow()) + Math.abs(adjacentLeft.getCol() - playerPos.getCol());
+        }
+        if (currentRoom.isValidPosition(currentPos.translateRight())) {
+            Position adjacentRight = currentPos.translateRight();
+            rightDistance = Math.abs(adjacentRight.getRow() - playerPos.getRow()) + Math.abs(adjacentRight.getCol() - playerPos.getCol());
+        }
+        return this.getSmallest(upDistance, downDistance, leftDistance, rightDistance, rotations);
+    }
+
+    private Rotation getSmallest(int up, int down, int left, int right, Rotation[] rotations) {
+        int[] adjacentArray = new int[]{up, down, left, right};
+        int smallestIndex = 0;
+        for (int i = 0; i < adjacentArray.length; i++) {
+            for (int j = i + 1; j < adjacentArray.length; j++) {
+                if (adjacentArray[i] > adjacentArray[j]) {
+                    smallestIndex = j;
+                } else {
+                    smallestIndex = i;
+                }
+            }
+        }
+        int direction = -1;
+        if (smallestIndex == 1) {
+            direction = 3;
+        } else if (smallestIndex == 2) {
+            direction = 2;
+        } else if (smallestIndex == 3) {
+            direction = 0;
+        } else if (smallestIndex == 0) {
+            direction = 1;
+        }
+        return rotations[direction];
     }
 }
