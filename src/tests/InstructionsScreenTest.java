7460f6596c17e0794bf203b7a7ab3ac20ad96ede
jzheng323 <jzheng323@gatech.edu>
2021-04-26 12:43:48 -0400

    Added game music and unit tests

---
+++ src/tests/InstructionsScreenTest.java
@@ -0,0 +1,24 @@
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import quack.controllers.InstructScreenController;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class InstructionsScreenTest extends ApplicationTest {
+    @Override
+    public void start(Stage stage) throws Exception {
+        InstructScreenController controller = new InstructScreenController(stage);
+        controller.initInstructScreen();
+    }
+    @Test
+    public void testInstructions() throws InterruptedException{
+        TimeUnit.SECONDS.sleep(1);
+        clickOn("Menu");
+        TimeUnit.SECONDS.sleep(1);
+        verifyThat("Play", NodeMatchers.isNotNull());
+    }
+}
